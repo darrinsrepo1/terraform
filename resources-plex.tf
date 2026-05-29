ae54ebbda10255db80a9dd5ecc8cff350f6740fe
darrinsrepo1 <dscheivecali@gmail.com>
2026-05-28 23:15:29 -0700

    fixed volume and mount issue, created fresh plex volume to resolve stale mount problem in plex app itself

--- resources-plex.tf
+++ resources-plex.tf
@@ -5,6 +5,10 @@ resource "docker_image" "plex" {
   name = var.plex_image
 }
 
+resource "docker_volume" "plex_config" {
+  name = "plex-config"
+}
+
 # __generated__ by Terraform from "1f89a31db0e1584cd64742710e478e8d77c8c0a4de95e1355efeb52f78ac9930"
 resource "docker_container" "plex" {
   cpu_period = null
@@ -14,9 +18,11 @@ resource "docker_container" "plex" {
   cpus       = null
   env = [
     "PUID=1000",
-    "PGID=1000",
+    "PGID=1001",
+    "PLEX_UID=1000",
+    "PLEX_GID=1001",
     "TZ=America/Los_Angeles",
-    "PLEX_CLAIM=${var.plex_claim}",
+    #    "PLEX_CLAIM=${var.plex_claim}",
     "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
     "TERM=xterm",
     "LANG=C.UTF-8",
@@ -30,7 +36,7 @@ resource "docker_container" "plex" {
   memory_swap        = 0
   must_run           = true
   name               = "tf-plex"
-  network_mode       = "host"
+#  network_mode       = "host"
   restart            = "unless-stopped"
   shm_size           = 64
 
@@ -68,28 +74,30 @@ resource "docker_container" "plex" {
     read_only = true
   }
 
-  mounts {
-    type   = "bind"
-    target = "/config"
-    source = "/docker/plex/dj-plex"
+  volumes {
+    volume_name = docker_volume.plex_config.name
+    #    host_path      = "/docker/plex/dj-plex" # Update this to your local media path
+    container_path = "/config"
   }
 
-  mounts {
-    type   = "bind"
-    target = "/mnt/tv"
-    source = "/mnt/vol1/media/shows"
+  #  mounts {
+  #    type      = "bind"
+  #    target    = "/media/shows"
+  #    source    = "/mnt/vol1/media/shows"
+  #  }
+  volumes {
+    host_path      = "/mnt/vol1/media/shows" # Update this to your local media path
+    container_path = "/media/shows"
   }
 
-  mounts {
-    type   = "bind"
-    target = "/mnt/movies"
-    source = "/mnt/vol1/media/movies"
+  volumes {
+    host_path      = "/mnt/vol1/media/movies" # Update this to your local media path
+    container_path = "/media/movies"
   }
 
-  mounts {
-    type   = "bind"
-    target = "/mnt/music"
-    source = "/mnt/vol1/media/music"
+  volumes {
+    host_path      = "/mnt/vol1/media/music" # Update this to your local media path
+    container_path = "/media/music"
   }
 
   healthcheck {
@@ -99,5 +107,46 @@ resource "docker_container" "plex" {
     start_period   = "0s"
     test           = ["CMD-SHELL", "/healthcheck.sh || exit 1"]
     timeout        = "2s"
+  }
+
+  ports {
+    internal = 32400
+    external = 32400
+    protocol = "tcp"
+  }
+  ports {
+    internal = 8324
+    external = 8324
+    protocol = "tcp"
+  }
+  ports {
+    internal = 32469
+    external = 32469
+    protocol = "tcp"
+  }
+  ports {
+    internal = 1900
+    external = 1900
+    protocol = "udp"
+  }
+  ports {
+    internal = 32410
+    external = 32410
+    protocol = "udp"
+  }
+  ports {
+    internal = 32412
+    external = 32412
+    protocol = "udp"
+  }
+  ports {
+    internal = 32413
+    external = 32413
+    protocol = "udp"
+  }
+  ports {
+    internal = 32414
+    external = 32414
+    protocol = "udp"
   }
 }
